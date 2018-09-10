9ee48fa9c2506f8ef0e33530cf8bdbef3e2d3fe4
Alexandre Peccard <alexandre.peccard@gmail.com>
2018-09-10 18:40:31 +0200

    Exercice 19

---
+++ exo_19.rb
@@ -0,0 +1,20 @@
+$prenoms = ["alexandre", "tiffany", "joseph", "marinne", "nicolas", "anièce", "pauline", "eric", "robert", "annaelle", "thomas"]
+$noms = ["martin", "dupuis", "loncle", "canagan", "perrault", "starling", 'gwen', "guesnier", "peccard", "corlay"]
+$domains = ["gmail","live", "hotmail", "wanadoo"]
+$exts = ["net", "fr", "com", "org"]
+
+def generateName()
+	return "#{$prenoms[rand(10)]}.#{$noms[rand(9)]}"
+end
+
+emailCount = 50
+i = 1
+emails = []
+while i <= emailCount do
+	address = generateName() + "." + sprintf('%02d', i.to_s) + "@" + $domains[rand(3)] + "." + $exts[rand(3)]
+	if i % 2 == 0
+		emails << address 
+	end
+	i +=1
+end
+puts "#{emails}"
