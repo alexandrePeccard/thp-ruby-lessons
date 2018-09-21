bc7d6bfcea482bdd35aeec94ee7a705435a9a439
Alexandre Peccard <alexandre.peccard@gmail.com>
2018-09-21 13:47:24 +0200

    Exercice 22 : la boucle des champions

---
+++ exo_22.rb
@@ -0,0 +1,39 @@
+print("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?\n")
+height = Integer(gets.chomp)
+i = 0
+j = 0
+chaine = ""
+puts "Voici la pyramide :"
+
+#J'ai un doute sur le sens demandé alors je mets les deux !
+
+# La pyramide de droite à gauche
+while i < height do
+    counter_empty = height - i
+    counter_symbol = height - counter_empty
+    chaine = ""
+    k = 0
+    j = 0
+
+    while k < (counter_empty -1) do
+        chaine += ' '
+        k += 1
+    end
+
+    while j <= counter_symbol do
+        chaine += '#'
+        j += 1
+    end
+    i += 1
+    puts"#{chaine}"
+end
+
+# La pyramide de gauche à droite
+# while i < height do
+# 	while j <= i do
+# 		chaine += "#"
+# 		j += 1
+# 	end
+# 	i += 1
+# 	puts "#{chaine}"
+# end
