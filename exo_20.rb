9eed0491711397702122f617fa31ae9b98d16d5b
Alexandre Peccard <alexandre.peccard@gmail.com>
2018-09-28 15:55:09 +0200

    add limit to the pyramid's height

--- exo_20.rb
+++ exo_20.rb
@@ -1,5 +1,9 @@
 print("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?\n")
 height = Integer(gets.chomp)
+if height > 25
+	height = 25
+	puts("La pyramide ne peut dépasser 25 étages !")
+end
 i = 0
 j = 0
 string = ""
